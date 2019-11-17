9e98a071a2b34a375dacdb2e327a6c1567c71dbc
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-11-17 22:16:23 +0300

    Add files via upload

---
+++ NET.W.2019.Kaminskiy.02/1.InsertNumber/InsertNumber.Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+
+
+namespace InsertBits.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        [TestCase(105, 37, 2, 7, ExpectedResult = 149)]
+        [TestCase(873, 37, 2, 7, ExpectedResult = 917)]
+        [TestCase(873, 37, 2, 9, ExpectedResult = 149)]
+        [TestCase(873, -37, 2, 11, ExpectedResult = 3949)]
+        
+        public int ValuesTest(int val1, int val2, int i, int j)
+        {
+            return Program.InsertNumber(val1, val2, i, j);
+        }
+
+        [TestCase(34,20,15,1)]
+        public void ExceptionTest(int val1, int val2, int i, int j)
+        {
+            Assert.That(() => Program.InsertNumber(val1, val2, i, j), Throws.TypeOf<ArgumentException>());
+        }
+    }
+}
---
+++ NET.W.2019.Kaminskiy.02/1.InsertNumber/InsertNumber.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertBits
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int x = InsertNumber(873, -37, 2, 2);
+                Console.WriteLine(x);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Method inserts y value bits to i..j bit range of the x value
+        /// </summary>
+        /// <param name="x">
+        /// First value, int
+        /// </param>
+        /// <param name="y">
+        /// Second value, int
+        /// </param>
+        /// <param name="i">
+        /// lower bit position
+        /// </param>
+        /// <param name="j">
+        /// higher bit position (j>=i)
+        /// </param>
+        /// <returns>
+        /// modified x;
+        /// </returns>
+
+        public static int InsertNumber(int x, int y, int i, int j)
+        {
+            
+            if (i > j || j > 31)
+            {
+                throw new ArgumentException();                
+            }
+
+            //Algorithm uses shifts. It appears that expression value<<32 has no effect whatsoever - value remains the same (compiler bug?). 
+            //Value <<31; Value<<1 works as expected
+            //integers are converted to unsigned for convenience. 
+            //If most significant bit of signed integer becomes 1 - value becomes negative and right shift fills bits with ones instead of zeroes.
+            //explicit conversion (uint)value does not affect bit structure of the value, only its type.
+
+
+            //The goal is to nulify bits in i..j range of the first value(x). 
+            //x is devided into 2 parts.
+            //First part preserves bits to the right of i:
+            uint right_x = (uint)x << 31 - i; right_x <<= 1; 
+            right_x >>= 31 - i; right_x >>= 1;
+            //Console.WriteLine(right_x);
+
+            //Second part preserves bits to the left of j
+            uint left_x = (uint)x >> j; left_x >>= 1; 
+            left_x <<= (j); left_x <<= 1;
+            //Console.WriteLine(left_x);
+            //Console.WriteLine((int)left_x);
+
+            //Nulify all bits of y except for i..j range
+            uint range_y = (uint)y<<i;
+            range_y <<= 31 - j;            
+            range_y >>= 31 - j;
+            //Console.WriteLine(range_y);
+                        
+            //All parts are united with | operation and converted back to signed integer
+            x = (int)(right_x | left_x | range_y);
+
+            return x;
+        }
+    }
+}
