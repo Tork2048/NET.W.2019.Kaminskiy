af6f612e314513bdebd55bfed5f078281aa3274c
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-11-21 19:05:53 +0300

    Add files via upload

---
+++ NET.W.2019.04/GCD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time;
+            int result = GCD_Calculation.GetGCD(GCD_Calculation.Alg_option.Stein, out time, 715, 627, 6259, 858,8679,28259);
+            Console.WriteLine(result);
+            Console.WriteLine($"Calculation time - {time} nanoseconds");
+            Console.ReadKey();
+        }
+    }
+}
