347a0fb21d3c6ae5ba1c16060339b440773e2a02
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-11-21 19:10:07 +0300

    Add files via upload

---
+++ NET.W.2019.04-2/DoubleToString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.MinValue;
+            Console.WriteLine(x.ConvertToString());
+
+            Console.ReadKey();
+        }
+    }
+}
