a89eb4ceef53415c55fe4bc08dd334ab131b5f04
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-11-26 21:49:24 +0300

    Add files via upload

---
+++ NET.W.2019.06/2.SortJaggedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SortJaggedArray.SortClass;
+
+namespace SortJaggedArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[][] a1 = new int[6][];
+            a1[0] = new int[] { 1, 2, 6, 0, 10 };
+            a1[1] = new int[] { 1, 2, 3 };
+            a1[2] = new int[] { 1, 2, 30, 4, 5 };
+            a1[3] = new int[] { 1, 2, 88 };
+            a1[4] = new int[] { 1, 2, 34 };
+            a1[5] = new int[] { 1, 2, 3, 4, 5 };
+            int[][] result = JuggedArraySort(a1, SortOption.ByMaxInRow, SortOrderOption.Descend);
+            SortClass.DisplayJaggedArray(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NET.W.2019.06/2.SortJaggedArray/Tests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SortJaggedArray.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private static TestData[] testarraySUM = new TestData[]
+            {
+                new TestData
+                (new int[][]
+                    {
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    new int[][]
+                    {
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+                    
+                    SortClass.SortOrderOption.Ascend,
+
+                    SortClass.SortOption.ByRowSum),
+
+                new TestData
+                (new int[][]
+                    {
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    new int[][]
+                    {
+                        new int[] { 45, 23, 3, 87 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                    },
+
+                    SortClass.SortOrderOption.Descend,
+
+                    SortClass.SortOption.ByRowSum),
+            };
+
+        private static TestData[] testarrayMin = new TestData[]
+            {
+                new TestData
+                (new int[][]
+                    {
+                        new int[] { -1, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    new int[][]
+                    {
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { -1, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    SortClass.SortOrderOption.Ascend,
+
+                    SortClass.SortOption.ByMinInRow),
+
+                new TestData
+                (new int[][]
+                    {
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    new int[][]
+                    {
+                        new int[] { 45, 23, 3, 87 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                    },
+
+                    SortClass.SortOrderOption.Descend,
+
+                    SortClass.SortOption.ByMinInRow),
+            };
+
+        private static TestData[] testarrayMax = new TestData[]
+            {
+                new TestData
+                (new int[][]
+                    {
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    new int[][]
+                    {
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },                                                
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    SortClass.SortOrderOption.Ascend,
+
+                    SortClass.SortOption.ByMaxInRow),
+
+                new TestData
+                (new int[][]
+                    {
+                        new int[] { 0, 4, 7, 1 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 45, 23, 3, 87 },
+                    },
+
+                    new int[][]
+                    {
+                        new int[] { 45, 23, 3, 87 },
+                        new int[] { -1, 9, 34, 9, 3, -44 },
+                        new int[] { 10, 6, 11, 20, 0, 3 },
+                        new int[] { 0, 4, 7, 1 },
+                    },
+
+                    SortClass.SortOrderOption.Descend,
+
+                    SortClass.SortOption.ByMaxInRow),
+            };
+
+        [Test, TestCaseSource("testarraySUM")]
+        public void TestSortBySumInRow(TestData testdata)
+        {
+            int[][] result = SortClass.JuggedArraySort(testdata.SourceArray, testdata.Option, testdata.OrderOption);
+            Assert.That(result, Is.EqualTo(testdata.ExpectedArray));
+        }
+
+        [Test, TestCaseSource("testarrayMin")]
+        public void TestSortByMinInRow(TestData testdata)
+        {
+            int[][] result = SortClass.JuggedArraySort(testdata.SourceArray, testdata.Option, testdata.OrderOption);
+            Assert.That(result, Is.EqualTo(testdata.ExpectedArray));
+        }
+
+        [Test, TestCaseSource("testarrayMax")]
+        public void TestSortByMaxInRow(TestData testdata)
+        {
+            int[][] result = SortClass.JuggedArraySort(testdata.SourceArray, testdata.Option, testdata.OrderOption);
+            Assert.That(result, Is.EqualTo(testdata.ExpectedArray));
+        }
+    }
+}
