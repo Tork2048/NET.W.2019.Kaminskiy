086d35a562e90643a0db806d51f7c8f32b261e79
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-12-10 19:17:57 +0300

    Add files via upload

---
+++ NET.W.2019.10-11/4.GCD_Refactoring/Program.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GCD
+{
+    using System;
+
+    /// <summary>
+    /// Class that contains entry poit (Main).
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args">
+        /// Start parameters.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            int time;
+            int result = GCD_Calculation.GetGCD(GCD_Calculation.GetGCDForTwo, out time, 715, 627, 6259, 858, 8679, 28259);
+            Console.WriteLine(result);
+            Console.WriteLine($"Calculation time - {time} nanoseconds");
+            Console.ReadKey();
+        }
+    }
+}
