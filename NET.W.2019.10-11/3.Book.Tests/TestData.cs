d88a1fa332cfc76f38db9b3df0361d3a902cd1a3
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-12-10 19:16:06 +0300

    Add files via upload

---
+++ NET.W.2019.10-11/3.Book.Tests/TestData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Tests
+{
+    public class TestData
+    {
+        public Book BookSource { get; set; }
+
+        public string ExpectedResult { get; set; }
+
+        public string Format { get; set; }
+
+
+    }
+}
