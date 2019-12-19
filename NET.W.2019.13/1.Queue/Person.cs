23012d57491fd03d59a903e11c914a96a21f0fe9
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-12-19 23:17:50 +0300

    Add files via upload

---
+++ NET.W.2019.13/1.Queue/Person.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="Person.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GenericCollectionQueue
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Test data class.
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        /// <param name="name">
+        /// class data - Name.
+        /// </param>
+        /// <param name="lastName">
+        /// class data - Last name.
+        /// </param>
+        /// <param name="age">
+        /// class data - Age.
+        /// </param>
+        public Person(string name, string lastName, int age)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        /// <summary>
+        /// Gets or Sets Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or Sets LastName.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Age.
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Method converts class data to string. Required for output.
+        /// </summary>
+        /// <returns>
+        /// Class data as a string.
+        /// </returns>
+        public override string ToString()
+        {
+            string output = $"{this.Name}, {this.LastName}, {this.Age}";
+            return output;
+        }
+    }
+}
