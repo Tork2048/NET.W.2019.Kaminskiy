508acb995d78080680d2d0b6a45118c83e06f568
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-12-19 23:20:48 +0300

    Add files via upload

---
+++ NET.W.2019.13/2.Matrix/DiagonalMatrix.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="DiagonalMatrix.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GenericMatrix
+{
+    using System;
+
+    /// <summary>
+    /// More specific matrix - diagonal.
+    /// Derived from SqaureMatrix.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Matrix data type.
+    /// </typeparam>
+    public class DiagonalMatrix<T> : SquareMatrix<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiagonalMatrix{T}"/> class.
+        /// Uses base constructor.
+        /// </summary>
+        /// <param name="array">
+        /// Given array.
+        /// </param>
+        public DiagonalMatrix(T[,] array)
+            : base(array)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiagonalMatrix{T}"/> class.
+        /// Uses base constructor.
+        /// </summary>
+        /// <param name="dimension">
+        /// Dimension to create diagonal array.
+        /// </param>
+        public DiagonalMatrix(int dimension)
+            : base(dimension)
+        {
+        }
+
+        /// <summary>
+        /// Validation logic for diagonal matrix.
+        /// </summary>
+        /// <param name="array">
+        /// Given array.
+        /// </param>
+        protected override void ValidateMatrix(T[,] array)
+        {
+            base.ValidateMatrix(array);
+
+            int rows = array.GetUpperBound(0) + 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    else if (this.comparer.Compare(array[i, j], default(T)) != 0)
+                    {
+                        throw new InvalidOperationException(message: "Array is not diagonal. Object cannot be constructed");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Element change  validation logic for diagonal matrix.
+        /// </summary>
+        /// <param name="indexI">
+        /// Row index.
+        /// </param>
+        /// <param name="indexJ">
+        /// Column index.
+        /// </param>
+        /// <param name="value">
+        /// Value to assign.
+        /// </param>
+        protected override void ChangeElement(int indexI, int indexJ, T value)
+        {
+            this.CheckIndex(indexI, indexJ);
+
+            if (indexI != indexJ && this.comparer.Compare(value, default(T)) != 0)
+            {
+                throw new ArgumentException(message: $"Element {value} at index ({indexI}, {indexJ}) is not compliant with diagonal matrix");
+            }
+
+            this.SetElement(indexI, indexJ, value);
+            string eventMessage = $"Element in Diagonal matrix at index ({indexI}, {indexJ}) has been changed to {value}";
+            this.OnElementChange(new ElementChangeArgs(indexI, indexJ, eventMessage));
+        }
+    }
+}
---
+++ NET.W.2019.13/2.Matrix/ElementChangeArgs.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="ElementChangeArgs.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GenericMatrix
+{
+    /// <summary>
+    /// Data class for element change event.
+    /// </summary>
+    public class ElementChangeArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ElementChangeArgs"/> class.
+        /// Constructs object with indexes and event message.
+        /// </summary>
+        /// <param name="i">
+        /// Row index.
+        /// </param>
+        /// <param name="j">
+        /// Column index.
+        /// </param>
+        /// <param name="message">
+        /// Event message.
+        /// </param>
+        public ElementChangeArgs(int i, int j, string message)
+        {
+            this.IndexI = i;
+            this.IndexJ = j;
+            this.EventMessage = message;
+        }
+
+        /// <summary>
+        /// Gets event message.
+        /// </summary>
+        public string EventMessage { get; }
+
+        /// <summary>
+        /// Gets row index.
+        /// </summary>
+        public int IndexI { get; }
+
+        /// <summary>
+        /// Gets column index.
+        /// </summary>
+        public int IndexJ { get; }
+    }
+}
