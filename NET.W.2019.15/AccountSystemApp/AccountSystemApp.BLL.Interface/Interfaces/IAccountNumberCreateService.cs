c88774ab14c394c12207bc6d5bf980ff2491c3e2
Tork2048 <56931120+Tork2048@users.noreply.github.com>
2019-12-26 00:21:37 +0300

    Add files via upload

---
+++ NET.W.2019.15/AccountSystemApp/AccountSystemApp.BLL.Interface/Interfaces/IAccountNumberCreateService.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="IAccountNumberCreateService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AccountSystemApp.BLL.Interface.Interfaces
+{
+    /// <summary>
+    /// Interface that provides access to Account number generator method.
+    /// Part of stairway pattern implementation.
+    /// </summary>
+    public interface IAccountNumberCreateService
+    {
+        /// <summary>
+        /// Description of Account number generator method.
+        /// </summary>
+        /// <param name="id">
+        /// Unique account id.
+        /// </param>
+        /// <returns>
+        /// Account number.
+        /// </returns>
+        public int GenerateAccountNumber(int id);
+    }
+}
