dc70c12f244845fa504b24c88c3916b020377266
Maksim Kaminskiy <McTork@gmail.com>
2020-01-17 16:29:25 +0300

    Tidy up Day4 homework

---
+++ NET.W.2019.04/Task1/GCD.Tests/Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GCD.Tests
+{
+    /// <summary>
+    /// Test class for unit testing.
+    /// </summary>
+    [TestFixture]
+    public class Tests
+    {
+        /// <summary>
+        /// GCD calculation method testing.
+        /// </summary>
+        /// <param name="option">
+        /// Algorithm option.
+        /// </param>
+        /// <param name="integers">
+        /// Numbers to calculate GCD for.
+        /// </param>
+        /// <returns>
+        /// GCD.
+        /// </returns>
+        [TestCase(GCDCalculation.AlgOption.Euclidean, 715, 627, 6259, 858, 8679, 28259, ExpectedResult = 11)]
+        [TestCase(GCDCalculation.AlgOption.Stein, 715, 627, 6259, 858, 8679, 28259, ExpectedResult = 11)]
+        [TestCase(GCDCalculation.AlgOption.Euclidean, 723078, 85, 5559, 15181, ExpectedResult = 17)]
+        [TestCase(GCDCalculation.AlgOption.Stein, 723078, 85, 5559, 15181, ExpectedResult = 17)]
+        public int TestGCDCalculation(GCDCalculation.AlgOption option, params int[] integers)
+        {
+            return GCDCalculation.GetGCD(option, integers);
+        }
+
+        /// <summary>
+        /// GCD exception test.
+        /// </summary>
+        /// <param name="option">
+        /// Algorithm option.
+        /// </param>
+        /// <param name="integers">
+        /// Numbers to calculate GCD for.
+        /// </param>
+        [TestCase(GCD.GCDCalculation.AlgOption.Euclidean, 0, 0, 0, 0, 0, 0, 0)]
+        [TestCase(GCD.GCDCalculation.AlgOption.Stein, 0, 0, 0, 0, 0, 0, 0)]
+        public void GCDExceptionTest(GCDCalculation.AlgOption option, params int[] integers)
+        {
+            Assert.That(() => GCDCalculation.GetGCD(option, integers), Throws.TypeOf<ArgumentException>());
+        }
+    }
+}
---
+++ NET.W.2019.04/Task2/DoubleToStringConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoubleToString
+{
+    /// <summary>
+    /// Contains entry point.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        public static void Main()
+        {
+            double x = double.MinValue;
+            Console.WriteLine(x.ConvertToString());
+
+            Console.ReadKey();
+        }
+    }
+}
