3acfcefabfe73b50f9f66a5b0d245452c631cf53
Maksim Kaminskiy <McTork@gmail.com>
2020-01-22 00:48:15 +0300

    Add Homework day21

---
+++ NET.W.2019.21/Task2 - SlideShowGallary/SlideShowGallary/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SlideShowGallary.Models;
+
+namespace SlideShowGallary.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Image> images = new List<Image>();
+            images.Add(new Image { ImageId = 1, Description = "The Woods", ImagePath = @"~/Images/img_woods_wide.jpg", ImageThumbPath = @"~/Images/img_woods.jpg", });
+            images.Add(new Image { ImageId = 2, Description = "Cinque Terre", ImagePath = @"~/Images/img_5terre_wide.jpg", ImageThumbPath = @"~/Images/img_5terre.jpg", });
+            images.Add(new Image { ImageId = 3, Description = "Mountains and fjords", ImagePath = @"~/Images/img_mountains_wide.jpg", ImageThumbPath = @"~/Images/img_mountains.jpg", });
+            images.Add(new Image { ImageId = 4, Description = "Northern Lights", ImagePath = @"~/Images/img_lights_wide.jpg", ImageThumbPath = @"~/Images/img_lights.jpg", });
+            images.Add(new Image { ImageId = 5, Description = "Nature and sunrise", ImagePath = @"~/Images/img_nature_wide.jpg", ImageThumbPath = @"~/Images/img_nature.jpg", });
+            images.Add(new Image { ImageId = 6, Description = "Snowy Mountains", ImagePath = @"~/Images/img_snow_wide.jpg", ImageThumbPath = @"~/Images/img_snow.jpg", });
+
+            ViewBag.Images = images;
+            ViewBag.Path = Url.Content("~/Images/Dawn.jpg");
+
+            return View();
+        }        
+    }
+}
---
+++ NET.W.2019.21/Task2 - SlideShowGallary/SlideShowGallary/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlideShowGallary.Models
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string ImageThumbPath { get; set; }
+    }
+}
